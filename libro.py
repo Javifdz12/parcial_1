5e3b364e211c7f5b30a5e84bdeb8d213c99d749b
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-05-01 21:44:50 +0200

    gg

--- libro.py
+++ libro.py
@@ -23,6 +23,7 @@ class libro:
                         self.num_paginas=num_paginas
             else:
                 self.num_paginas=0
+
         if isinstance(genero,str) and genero.upper() in generos:
             self.genero=genero
         else:
@@ -31,15 +32,18 @@ class libro:
         if isinstance(autor,str):
             self.autor=autor
         else:
-            self.genero="autor"
+            self.autor="autor"
+
         if isinstance(editorial,str):
             self.editorial=editorial
         else:
-            self.editorial="autor"
+            self.editorial="editorial"
+
         if isinstance(titulo,str):
             self.titulo=titulo
         else:
             self.titulo="titulo"
+
     def get_num_paginas(self):
         print(f"el numero de paginas es {self.num_paginas}")
     def get_genero(self):
@@ -50,18 +54,59 @@ class libro:
         print(f"se titula: {self.titulo}")
     def get_editorial(self):
         print(f"la editorial es: {self.editorial}")
+
     def set_num_paginas(self):
-        self.num_paginas=input("cuantas paginas tiene el libro: ")
+        num_paginas=input("cuantas paginas tiene el libro: ")
+        if isinstance(num_paginas,int):
+            if num_paginas<0:
+                num_paginas=input("numero de paginas incorrecto debe ser numero positivo: ")
+                if isinstance(num_paginas,int):
+                    if num_paginas<0:
+                        self.num_paginas=0
+                    else:
+                        self.num_paginas=num_paginas
+                else:
+                    self.num_paginas=0
+            else:
+                self.num_paginas=num_paginas
+        else:
+            num_paginas=input(f"{num_paginas} no es un numero de paginas introduce entero positivo: ")
+            if isinstance(num_paginas,int):
+                    if num_paginas<0:
+                        self.num_paginas=0
+                    else:
+                        self.num_paginas=num_paginas
+            else:
+                self.num_paginas=0
     def set_genero(self):
-        self.genero=input("cual es el genero del libro: ")
+        genero=input("cual es el genero del libro: ")
+        if isinstance(genero,str):
+            self.genero=genero
+        else:
+            self.genero="genero"
     def set_autor(self):
-        self.autor=input("cual es el autor del libro: ")
+        autor=input("cual es el autor del libro: ")
+        if isinstance(autor,str):
+            self.autor=autor
+        else:
+            self.autor="autor"
     def set_titulo(self):
-        self.titulo=input("cual es el titulo del libro: ")
+        titulo=input("cual es el titulo del libro: ")
+        if isinstance(titulo,str):
+            self.titulo=titulo
+        else:
+            self.titulo="titulo"
     def set_editorial(self):
-        self.editorial=input("cual es la editorial del libro: ")
+        editorial=input("cual es la editorial del libro: ")
+        if isinstance(editorial,str):
+            self.editorial=editorial
+        else:
+            self.editorial="editorial"
+    def descripcion(self):
+        print(f"el libro titulado {self.titulo},de la editorial {self.editorial} ,esta escrito por {self.autor}, pertenece a {self.genero} y tiene {self.num_paginas} paginas")
+
 
 
 
 libro1=libro(25,"novela aventuras","tu prima","sm","los cinco")
-libro1.get_genero()
+libro1.descripcion()
