97b9de6a4e50070f1ed72b27a96e19a23f32c9b3
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-05-01 23:21:42 +0200

    gg

--- ejercicio_relaciones_animales_jeje.py
+++ ejercicio_relaciones_animales_jeje.py
@@ -1,31 +1,38 @@
 class animal:
-    def __init__(self,tipo,reproduccion):
+    def __init__(self,tipo,reproduccion,sexo,especie,color):
         self.tipo=tipo
         self.reproduccion=reproduccion
-class mamifero:
-    print("soy mamifero")
-class ave:
-    print("soy un ave")
-class oviparo:
-    print("soy oviparo")
-class viviparo:
-    print("soy viviparo")
-class pato(animal):
-    def __init__(self,tipo,reproduccion,especie,color):
-        animal.__init__(self,tipo,reproduccion)
+        sexos=["masculino","femenino"]
+        if sexo.lower() in sexos:
+            self.sexo=sexo
+        else:
+            print("sexo incorrecto")
         self.especie=especie
         self.color=color
+class mamifero(animal):
+    def __init__(self,sexo,especie,color,reproduccion):
+        animal.__init__(self,sexo,especie,color,reproduccion)
+class ave(animal):
+    def __init__(self,sexo,especie,color,reproduccion):
+        animal.__init__(self,sexo,especie,color,reproduccion)
+class oviparo(mamifero,ave):
+    def __init__(self,sexo,especie,color):
+        ave.__init__(self,sexo,especie,color)
 
-class gato(animal):
-    def __init__(self,tipo,reproduccion,especie,color):
-        animal.__init__(self,tipo,reproduccion)
-        self.especie=especie
-        self.color=color
-class ornitorrinco(animal):
-    def __init__(self,tipo,reproduccion,color):
-        animal.__init__(self,tipo,reproduccion)
-        self.color=color
+class viviparo(mamifero):
+    def __init__(self,sexo,especie,color):
+        mamifero.__init__(self,sexo,especie,color)
+class pato(oviparo):
+    def __init__(self,especie,color,sexo,nombre):
+        oviparo.__init__(self,especie,color,sexo)
+        self.nombre = nombre
 
-pato1=pato(ave(),oviparo(),"anatida","verde")
-#gato1=gato(mamifero(),viviparo(),"egipcio","rosa")
-#ornitorrinco1=ornitorrinco(mamifero(),oviparo(),"marron")
+class gato(viviparo):
+    def __init__(self,especie,color,sexo,nombre):
+        viviparo.__init__(self,especie,color,sexo)
+        self.nombre = nombre
+class ornitorrinco(oviparo):
+    def __init__(self,sexo,color,nombre):
+        oviparo.__init__(self,color,sexo)
+        self.nombre = nombre
+
